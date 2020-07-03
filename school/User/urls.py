3ee4e73481be4bc322832cbe1deebf82171a7df0
Nitka Gupta <nitikagupta208@gmail.com>
2020-07-03 13:25:51 +0530

    Login Logout Working

--- school/User/urls.py
+++ school/User/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+app_name = "User"
 
 urlpatterns = [
     path('', views.HomeView, name='home'),
-    path('about/', views.AboutUsView.as_view(), name='about'),
+    path('about/', views.AboutUsView, name='about'),
     path('signup/', views.SignUpView, name='signup'),
     path('login/', views.LoginView, name='login'),
     path('logout/',views.LogoutView, name='logout'),
--- school/User/views.py
+++ school/User/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 
 def HomeView(request):
-    if Userobject.is_authenticated:
+    if request.user.is_authenticated:
         print("momo")
     return render(request,'home.html')
 
@@ -15,25 +15,29 @@ class AboutUsView(TemplateView):
     template_name = 'about.html'
 
 def SignUpView(request):
+    list = Userobject.object.all()
+    print("Here is the list")
+    print(list)
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password']
             email = form.cleaned_data['email']
             name = form.cleaned_data['name']
+            print("email = " + str(email) + " password = "  + str(password))
             user = Userobject.object.create_user(
                 email=email, password=password,name = name,
             )
             messages.success(
                 request, "Registeration Successfull"
             )
-            return redirect('User:home')
+            return redirect('User: home')
         else:
             if Userobject.object.is_email_registered(email = request.POST['email']):
                 # messages.error(
                 #     request, 'Given Email address is already registered'
                 # )
-                print("Given Email is registered")
+                print("Given Email is registerede")
             else:
                 print("Incorrect Details")
                 # messages.error(request, 'Incorrect Details')
@@ -45,21 +49,32 @@ def LoginView(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
+            print("form is valid")
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             obj = Userobject.object.filter(email=email).first()
+            print("user = " + str(obj))
+            if obj.check_password(password):
+                print("none")
+            else : 
+                print("else")
+
+            print("the passowrd is " + str(password))
             if obj is not None:
-                obj = authenticate(email=email, password=password)
+                obj = authenticate(request , username = email, password = password)
+                print("after autjentication = " + str(obj))
                 if obj is not None:
                     login(request, obj)
-                    return redirect('User : home')
+                    return HttpResponse('Activation link is valid!')
                 else:
                     print("Incorrect Username")
             else:
                 print("Incorrect Username")
         else:
+            print("invalid")
             print("Please enter valid data")
     else:
+        
         form = LoginForm()
     return render(request, 'login.html', {'form':form})
 
